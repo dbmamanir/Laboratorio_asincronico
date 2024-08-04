6c108ccf9cdfa05510d7bc3ab2ddbe946a52f7d1
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-08-04 18:50:57 -0500

    iniciacion del registro de piezas

---
+++ Inventario.cpp
@@ -0,0 +1,8 @@
+#include "Inventario.h"
+#include <iostream>
+using namespace std;
+
+void insertarpiezas(Piezas pieza []){
+	pieza[0] = {"4255563",2,""};
+	//añadir las demas piezas para el registro
+}
