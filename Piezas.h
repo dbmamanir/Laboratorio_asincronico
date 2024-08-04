9258ed256dde45d0101250667a2f6b4cd66e2b8a
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-08-04 17:42:00 -0500

    se añade un archivo.h de la estructura piezas en nueva rama

---
+++ Piezas.h
@@ -0,0 +1,12 @@
+#ifndef PIEZAS_H
+#define PIEZAS_H
+
+#include <string>
+
+struct {
+	int cod;  //codigo
+	int cant;  //cantidad de piezas
+	std::string obs;  //observacion de la pieza
+} Pieza;
+
+#endif //piezas
