fafae1ea5b75241fc2d403fd81a44c25e8ef66fd
asocualayaf <asocualayaf@unjbg.edu.pe>
2024-08-05 03:16:04 -0500

    Añadiendo funcion de la opcion 3 del menu principal

---
+++ IngresarObsKit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string ObsKit = "Sin observaciones Generales";
+int main(){
+	cout<<"Ingresa la observacion del kit: ";
+	cin>> ObsKit;
+	cout<<ObsKit;//probar si guarda correctamente la variable
+	return 0;
+}
