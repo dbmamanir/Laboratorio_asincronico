199ece16cf9214693448101b309005ff4ebae2b5
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-08-05 00:02:47 -0500

    Se añade el funcionamiento de la opcion 1, mostrar lista completa

--- Inventario.cpp
+++ Inventario.cpp
@@ -6,3 +6,13 @@ void insertarpiezas(Pieza pieza[]){
 	pieza[0] = {"4255563",2,""};
 	//añadir las demas piezas para el registro
 }
+
+void mostrarLista(const Pieza piezas []){
+	for (int i=0; i<MAX_PIEZAS; i++){
+		cout<<"Codigo:"<<piezas[i].cod<<", Cantidad:"<<piezas[i].cant<<", Observaciones:"<<piezas[i].obs<<endl;
+	}
+}
+
+void mostrarCantidad(const Pieza piezas []){
+	
+}
