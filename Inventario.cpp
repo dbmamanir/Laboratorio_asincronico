f80baa178bae7049c68b63877e176c02198f3ba1
asocualayaf <asocualayaf@unjbg.edu.pe>
2024-08-08 23:06:08 -0500

    Eliminando los cpp innecesarios debido a que se usara en el mismo archivo de cabecera

--- Inventario.cpp
+++
@@ -1,8 +0,0 @@
-#include "Inventario.h"
-#include <iostream>
-using namespace std;
-
-void insertarpiezas(Pieza pieza[]){
-	pieza[0] = {"4255563",2,""};
-	//añadir las demas piezas para el registro
-}
