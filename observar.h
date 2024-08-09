baab1cdec9d8b323aa7b366fbaf1245d91b0ed61
asocualayaf <asocualayaf@unjbg.edu.pe>
2024-08-09 01:54:42 -0500

    Se añade el archivo que contiene el codigo correspondiente a observaciones y toma de fecha exacta

---
+++ observar.h
@@ -0,0 +1,30 @@
+#ifndef OBSERVAR_H
+#define OBSERVAR_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <ctime>
+
+#include "tiempo.h"
+using namespace std;
+
+
+void ingresarObservacion(vector<Observacion>& observaciones) {
+    Observacion nuevaObservacion;
+    std::string txt;
+    cout << "Introduce la observacion del kit: ";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::getline(std::cin, txt);
+
+    // Obtener la fecha y hora actual
+    nuevaObservacion.texto = txt;
+    nuevaObservacion.timestamp = chrono::system_clock::now();
+
+    // Almacenar la observacion
+    observaciones.push_back(nuevaObservacion);
+    cout << "Se incorporo una nueva observacion del kit.\n";
+}
+
+#endif
